db57ce26d83cbd168ec088944d27d579191ce99e
Om Kale <omkale0107@gmail.com>
2026-07-03 23:50:38 +0530

    initial commit

---
+++ Introduction/first.js
@@ -0,0 +1,11 @@
+
+const name = "Om Kale";
+const education = "MCA Student";
+const language = "JavaScript";
+
+console.log("Hello! ");
+console.log("My name is " + name + ".");
+console.log("I am an " + education + ".");
+console.log("I am learning " + language + ".");
+console.log("I love building web applications.");
+console.log("Thank you for visiting my GitHub! ");
